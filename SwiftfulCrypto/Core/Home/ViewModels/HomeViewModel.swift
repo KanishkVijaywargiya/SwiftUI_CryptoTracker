3dd72e1e83ad75f1cd33a93f77d0e888a1bea79c
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2022-04-04 18:17:53 +0530

    Sorting list data

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -14,11 +14,16 @@ class HomeViewModel: ObservableObject {
     @Published var portfolioCoins: [CoinModel] = []
     @Published var isLoading: Bool = false
     @Published var searchText: String = ""
+    @Published var sortOption: SortOption = .holdings
     
     private let coinDataService = CoinDataService.instance
     private let marketDataService = MarketDataService.instance
     private let portfolioDataService = PortfolioDataService.instance
     private var cancellables = Set<AnyCancellable>()
+    
+    enum SortOption {
+        case rank, rankReversed, holdings, holdingsReversed, price, priceReversed
+    }
     
     init() {
         addSubscibers()
@@ -26,11 +31,13 @@ class HomeViewModel: ObservableObject {
     
     func addSubscibers() {
         // updates allCoins
-        $searchText
-            .combineLatest(coinDataService.$allCoins)
+        $searchText // we are subscibe to allCoins & sortOption publishers.
+            .combineLatest(coinDataService.$allCoins, $sortOption)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
-            .map(filterCoins)
+            .map(filterAndSortCoins)
             .sink { [weak self] returnedCoins in
+                HapticManager.instance.notification(type: .success)
+                HapticManager.instance.impact(style: .heavy)
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
@@ -40,7 +47,10 @@ class HomeViewModel: ObservableObject {
             .combineLatest(portfolioDataService.$savedEntities)
             .map(mapAllCoinsToPortfolioCoins)
             .sink { [weak self] returnedCoins in
-                self?.portfolioCoins = returnedCoins
+                HapticManager.instance.notification(type: .success)
+                HapticManager.instance.impact(style: .heavy)
+                guard let self = self else { return }
+                self.portfolioCoins = self.sortPortfolioCoinsIfNeeded(coins: returnedCoins)
             }
             .store(in: &cancellables)
         
@@ -67,6 +77,13 @@ class HomeViewModel: ObservableObject {
         HapticManager.instance.impact(style: .heavy)
     }
     
+    private func filterAndSortCoins(text: String, coins: [CoinModel], sort: SortOption) -> [CoinModel] {
+        var updatedCoins = filterCoins(text: text, coins: coins)
+        // sort
+        sortCoins(sort: sort, coins: &updatedCoins) // here & sign means inout
+        return updatedCoins
+    }
+    
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
         guard !text.isEmpty else { return coins }
         let lowercasedText = text.lowercased()
@@ -74,6 +91,34 @@ class HomeViewModel: ObservableObject {
             return coin.name.lowercased().contains(lowercasedText) ||
             coin.symbol.lowercased().contains(lowercasedText) ||
             coin.id.lowercased().contains(lowercasedText)
+        }
+    }
+    
+    /*
+     private func sortCoins(sort: SortOption, coins: [CoinModel]) -> [CoinModel] {
+     here returning & taking in same [CoinModel] array. So we can use inout.
+     so that it won't make any new array which is same.
+     .sorted(by: ) this func creates or rethrow a new [] but
+     .sort(by: ) this func don't creates or rethrow a new []. This is used when same [] is using.
+     */
+    private func sortCoins(sort: SortOption, coins: inout [CoinModel]) {
+        switch sort {
+        case .rank, .holdings: coins.sort(by: {$0.rank < $1.rank})
+        case .rankReversed, .holdingsReversed: coins.sort(by: {$0.rank > $1.rank})
+        case .price: coins.sort(by: {$0.currentPrice > $1.currentPrice})
+        case .priceReversed: coins.sort(by: {$0.currentPrice < $1.currentPrice})
+        }
+    }
+    
+    /*
+     here we can not do inout because we are going to be calling it from this returnedCoin & this value is coming up from sink. & so it is not mutable. We can't actually change this vaule itself. So we need to create a new array here.
+     */
+    private func sortPortfolioCoinsIfNeeded(coins: [CoinModel]) -> [CoinModel] {
+        // can only be sort by holdings & reversedHoldings if needed.
+        switch sortOption {
+        case .holdings: return coins.sorted(by: {$0.currentHoldingsValue > $1.currentHoldingsValue})
+        case .holdingsReversed: return coins.sorted(by: {$0.currentHoldingsValue < $1.currentHoldingsValue})
+        default: return coins
         }
     }
     
