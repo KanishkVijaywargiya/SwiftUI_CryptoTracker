a553b95897f2ad85f38dbe0cd87d96ba5e36ef53
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2022-04-05 21:45:08 +0530

    display downloaded data in a grid format in detail screen.

--- SwiftfulCrypto/Core/Detail/Views/DetailView.swift
+++ SwiftfulCrypto/Core/Detail/Views/DetailView.swift
@@ -20,27 +20,96 @@ struct DetailLoadingView: View {
 }
 
 struct DetailView: View {
-    let coin: CoinModel
+    @StateObject private var vm: DetailViewModel
+    
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    private let spacing: CGFloat = 30
     
     init(coin: CoinModel) {
-        self.coin = coin
-        print("initializing the detail view for \(coin.name)")
+        _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
     }
     
     var body: some View {
-        ZStack {
-            if let coin = coin {
-                Text(coin.name)
+        ScrollView {
+            VStack(spacing: 20) {
+                Text("1")
+                    .frame(height: 150)
+                
+                overviewTitle
+                Divider()
+                overviewGrid
+                
+                additionalTitle
+                Divider()
+                additionalGrid
             }
+            .padding()
         }
+        .navigationTitle(vm.coin.name)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: dev.coin)
+        NavigationView {
+            DetailView(coin: dev.coin)
+        }
     }
 }
+
+extension DetailView {
+    private var overviewTitle: some View {
+        Text("Overview")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    private var overviewGrid: some View {
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: []) {
+                ForEach(vm.overviewStatistics) { stat in
+                    StatisticView(stat: stat)
+                }
+            }
+    }
+    private var additionalTitle: some View {
+        Text("Additional Detail")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    private var additionalGrid: some View {
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: []) {
+                ForEach(vm.additionalStatistics) { stat in
+                    StatisticView(stat: stat)
+                }
+            }
+    }
+}
+
+/* when this dosen't work
+ @StateObject var vm = DetailViewModel(coin: <#T##CoinModel#>)
+ let coin: CoinModel
+ 
+ both these statements are getting initialize at the same time. So this is not possible to pass in vm.
+ So use init() {...}
+ init(coin: CoinModel) {
+ ----- _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
+ print("initializing the detail view for \(coin.name)")
+ }
+ */
 
 /*
  @Binding var coin: CoinModel?
@@ -51,9 +120,9 @@ struct DetailView_Previews: PreviewProvider {
  }
  
  struct DetailView_Previews: PreviewProvider {
-     static var previews: some View {
-         DetailView(coin: .constant(dev.coin))
-     }
+ static var previews: some View {
+ DetailView(coin: .constant(dev.coin))
  }
-
+ }
+ 
  */
---
+++ SwiftfulCrypto/Services/CoinDetailDataService.swift
@@ -0,0 +1,33 @@
+//
+//  CoinDetailDataService.swift
+//  SwiftfulCrypto
+//
+//  Created by KANISHK VIJAYWARGIYA on 05/04/22.
+//
+
+import Foundation
+import Combine
+
+class CoinDetailDataService {
+    @Published var coinDetails: CoinDetailModel? = nil
+    var coinDetailSubsciption: AnyCancellable?
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        getCoinDetails()
+    }
+    
+    //downloading the data -> passing the id to url, eg:- bitcoin
+    func getCoinDetails() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/\(coin.id)?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false") else { return }
+        
+        coinDetailSubsciption = NetworkingManager.download(url: url)
+            .decode(type: CoinDetailModel.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedCoinDetails in
+                guard let self = self else { return }
+                self.coinDetails = returnedCoinDetails
+                self.coinDetailSubsciption?.cancel()
+            })
+    }
+}
